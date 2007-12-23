b7d77fdb4e0d19e890c4223d620a93336c8d4cd5
petr1fied <petr1fied@8c072486-f8c7-49fb-bba5-1a7a9737b6d2>
2007-12-23 22:30:28 +0000

    SMF logout bug fix

--- logout.php
+++ logout.php
@@ -40,13 +40,9 @@ logoutcookie();
 dbconn();
 
 if ($GLOBALS["FORUMLINK"]=="smf")
-  {
-    $language2=$language;
+{
     require($THIS_BASEPATH.'/smf/SSI.php');
-    require($THIS_BASEPATH.'/smf/Settings.php');
     require($THIS_BASEPATH.'/smf/Sources/Subs-Auth.php');
-    $language=$language2;
-
     setLoginCookie(-3600, 0);
 }
 
