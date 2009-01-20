dca3f79f2bcd02142f826eb417c7d50c37a32eb5
jm_clement <jm_clement@8c072486-f8c7-49fb-bba5-1a7a9737b6d2>
2009-01-20 17:39:21 +0000

    Fix: default value for PM was to not send, just debug...

--- admin/admin.masspm.php
+++ admin/admin.masspm.php
@@ -57,7 +57,7 @@ $footer="\n\n".'This is an automated system please do not reply!!!'; # "\n" = ne
 
 ## !!!!!*****DEBUG MODE*****!!!!!
 # Do we actually send the PM's ? usage: true or false
-$pm = false;
+$pm = true;
 # Recommended modes for debug: $pm = false; $list_users = true;
 
 # END OF SETTINGS, DO NOT EDIT BELOW UNLESS YOU KNOW WHAT YOU ARE DOING!!!!
@@ -77,152 +77,152 @@ $masspm_post=false;
 $ratio_d='';
 switch ($action) {
   case 'post':
-	  if (isset($_POST['masspm'])) {
-			#init vars
-			$ratio=(isset($_POST['ratio'])?$_POST['ratio']:0);
-			$pick=(isset($_POST['pick'])?$_POST['pick']:0);
-			$flevel=(int)($_POST['level']);
-			$tlevel=(int)($_POST['levell']);
-			$subject=($_POST['subject']==''?$default_subject:$_POST['subject']);
-			$original_subject=htmlspecialchars($subject);
-			$msg=(isset($_POST['msg'])?$_POST['msg']:'');
-			$subject=sqlesc($subject);
-			# empty message and not debug mode
-			if ($msg=='' && $pm) {
-				$error='return';
-			} else {
-				# append footer
-				if($footer)
-					$msg.=$footer;
-				$original_msg=$msg;
-				$msg=sqlesc($msg);
-				# get ratio
-				if ($ratio)
-					switch($pick) {
-						case 0:
-							$pick='='.$ratio;
-							$ratio_d='with a '.$language['RATIO'].' of <b>('.$ratio.')</b>';
-							break;
+      if (isset($_POST['masspm'])) {
+            #init vars
+            $ratio=(isset($_POST['ratio'])?$_POST['ratio']:0);
+            $pick=(isset($_POST['pick'])?$_POST['pick']:0);
+            $flevel=(int)($_POST['level']);
+            $tlevel=(int)($_POST['levell']);
+            $subject=($_POST['subject']==''?$default_subject:$_POST['subject']);
+            $original_subject=htmlspecialchars($subject);
+            $msg=(isset($_POST['msg'])?$_POST['msg']:'');
+            $subject=sqlesc($subject);
+            # empty message and not debug mode
+            if ($msg=='' && $pm) {
+                $error='return';
+            } else {
+                # append footer
+                if($footer)
+                    $msg.=$footer;
+                $original_msg=$msg;
+                $msg=sqlesc($msg);
+                # get ratio
+                if ($ratio)
+                    switch($pick) {
+                        case 0:
+                            $pick='='.$ratio;
+                            $ratio_d='with a '.$language['RATIO'].' of <b>('.$ratio.')</b>';
+                            break;
 
-						case 1:
-							$pick='>='.$ratio;
-							$ratio_d='with a '.$language['RATIO'].' of <b>('.$ratio.')</b> and above';
-							break;
-						case 2:
-							$pick='<='.$ratio;
-							$ratio_d='with a '.$language['RATIO'].' of <b>('.$ratio.')</b> and below';
-							break;
-					}
-				# get level
-				if ($flevel==0||$tlevel==0) {
-					# selected all
-					$where='WHERE u.id>1';
-					$rank_details='in all '.$language['USER_LEVEL'].'s';
-				} else {
-					# get id_level names
-					$where='id_level='.$flevel;
-					if ($flevel==$tlevel) {
-						$limit=1;
-					} else {
-						$limit=2;
-						$where.=' OR id_level='.$tlevel;
-						if ($flevel>$tlevel) {
-							$flevel+=$tlevel;
-							$tlevel=$flevel-$tlevel;
-							$flevel-=$tlevel;
-						}
-					}
-					$levelsRes=get_result('SELECT id_level, level FROM '.$TABLE_PREFIX.'users_level WHERE '.$where.' LIMIT '.$limit);
-					foreach ($levelsRes as $level)
-						$levels[$level['id_level']]= $level['level'];
-					# create query for actual user listing
-					if ($limit==2) {
-						$where='WHERE u.id_level BETWEEN '.$flevel.' AND '.$tlevel.' AND u.id>1';
-						$rank_details='in '.$language['USER_LEVEL'].'s <b>('.$levels[$flevel].' - '.$levels[$tlevel].')</b>';
-					} else {
-						$where='WHERE u.id_level='.$flevel.' AND u.id>1';
-						$rank_details='in '.$language['USER_LEVEL'].' <b>('.$levels[$flevel].')</b>';
-					}
-				}
-				# correct ratio value
-				if ($XBTT_USE) {
-					$tables=$TABLE_PREFIX.'users u LEFT JOIN xbt_users x ON x.uid=u.id';
-					if ($ratio)
-						$where.=' AND ((u.downloaded+IFNULL(x.downloaded,0))/(u.uploaded+IFNULL(x.uploaded,0.1)))'.$pick;	
-				} else {
-					$tables=$TABLE_PREFIX.'users u';
-					if ($ratio)
-						$where.=' AND ((u.downloaded)/(u.uploaded=0))'.$pick;
-				}
-				# get data
-				$pm_users=get_result('SELECT u.id, u.username FROM '.$tables.' '.$where,true);
-				$i=0;
-				# revamp data
-				foreach ($pm_users as $cur) {
-					if ( (!$pm_sender) && $cur['id']==$CURUSER['uid'])
-						continue;
-					$i++;
-					if ($pm)
-						send_pm($sender,$cur['id'],$subject,$msg);
-					if ($list_users)
-						$l_users[] ='<a href="'.$BASEURL.'/index.php?page=userdetails&amp;id='.$cur['id'].'">'.$cur['username'].'</a>';
-				}
-				# set output vars
-				$block_title=$language['MASS_SENT'];
-				$masspm_post=true;
-				$masspm['subject']=$original_subject;
-				$masspm['body']=format_comment($original_msg);
-				$masspm['info']='<b>'.$i.'</b> '.$language['USERS_FOUND'].' '.$rank_details.' '.$ratio_d.' !! '.((!$pm)?' [ DEBUG MODE ] ':'').'<br /><br />'.$language['USERS_PMED'].'<br />'.implode(' - ',$l_users);
-				break;
-			}			
-		}
+                        case 1:
+                            $pick='>='.$ratio;
+                            $ratio_d='with a '.$language['RATIO'].' of <b>('.$ratio.')</b> and above';
+                            break;
+                        case 2:
+                            $pick='<='.$ratio;
+                            $ratio_d='with a '.$language['RATIO'].' of <b>('.$ratio.')</b> and below';
+                            break;
+                    }
+                # get level
+                if ($flevel==0||$tlevel==0) {
+                    # selected all
+                    $where='WHERE u.id>1';
+                    $rank_details='in all '.$language['USER_LEVEL'].'s';
+                } else {
+                    # get id_level names
+                    $where='id_level='.$flevel;
+                    if ($flevel==$tlevel) {
+                        $limit=1;
+                    } else {
+                        $limit=2;
+                        $where.=' OR id_level='.$tlevel;
+                        if ($flevel>$tlevel) {
+                            $flevel+=$tlevel;
+                            $tlevel=$flevel-$tlevel;
+                            $flevel-=$tlevel;
+                        }
+                    }
+                    $levelsRes=get_result('SELECT id_level, level FROM '.$TABLE_PREFIX.'users_level WHERE '.$where.' LIMIT '.$limit);
+                    foreach ($levelsRes as $level)
+                        $levels[$level['id_level']]= $level['level'];
+                    # create query for actual user listing
+                    if ($limit==2) {
+                        $where='WHERE u.id_level BETWEEN '.$flevel.' AND '.$tlevel.' AND u.id>1';
+                        $rank_details='in '.$language['USER_LEVEL'].'s <b>('.$levels[$flevel].' - '.$levels[$tlevel].')</b>';
+                    } else {
+                        $where='WHERE u.id_level='.$flevel.' AND u.id>1';
+                        $rank_details='in '.$language['USER_LEVEL'].' <b>('.$levels[$flevel].')</b>';
+                    }
+                }
+                # correct ratio value
+                if ($XBTT_USE) {
+                    $tables=$TABLE_PREFIX.'users u LEFT JOIN xbt_users x ON x.uid=u.id';
+                    if ($ratio)
+                        $where.=' AND ((u.downloaded+IFNULL(x.downloaded,0))/(u.uploaded+IFNULL(x.uploaded,0.1)))'.$pick;   
+                } else {
+                    $tables=$TABLE_PREFIX.'users u';
+                    if ($ratio)
+                        $where.=' AND ((u.downloaded)/(u.uploaded=0))'.$pick;
+                }
+                # get data
+                $pm_users=get_result('SELECT u.id, u.username FROM '.$tables.' '.$where,true);
+                $i=0;
+                # revamp data
+                foreach ($pm_users as $cur) {
+                    if ( (!$pm_sender) && $cur['id']==$CURUSER['uid'])
+                        continue;
+                    $i++;
+                    if ($pm)
+                        send_pm($sender,$cur['id'],$subject,$msg);
+                    if ($list_users)
+                        $l_users[] ='<a href="'.$BASEURL.'/index.php?page=userdetails&amp;id='.$cur['id'].'">'.$cur['username'].'</a>';
+                }
+                # set output vars
+                $block_title=$language['MASS_SENT'];
+                $masspm_post=true;
+                $masspm['subject']=$original_subject;
+                $masspm['body']=format_comment($original_msg);
+                $masspm['info']='<b>'.$i.'</b> '.$language['USERS_FOUND'].' '.$rank_details.' '.$ratio_d.' !! '.((!$pm)?' [ DEBUG MODE ] ':'').'<br /><br />'.$language['USERS_PMED'].'<br />'.implode(' - ',$l_users);
+                break;
+            }           
+        }
 
-	case 'write':
-		switch ($error) {
-		  case 'return':
-				$error_msg=$language['MASS_PM_ERROR'];
-				$error=true;
-				break;
+    case 'write':
+        switch ($error) {
+          case 'return':
+                $error_msg=$language['MASS_PM_ERROR'];
+                $error=true;
+                break;
 
-		  default:
-				$error=false;
-		}
-		# init options
-		$opts['name']='level';
-		$opts['complete']=true;
-		$opts['id']='id';
-		$opts['value']='level';
-		$opts['default']=$flevel;
-		# get from rank group
-		$ranks=rank_list();
-		$ranks[]=array('id'=>0, 'level'=>$language['ALL']);
-		$masspm['combo_from_group']=get_combo($ranks, $opts);
-		# get to rank group
-		$opts['name']='levell';
-		$opts['default']=$tlevel;
-		$masspm['combo_to_group']=get_combo($ranks, $opts);
-		# get ratios
-		$combo="\n".'<select name="ratio"><option value="0"'.($ratio==0?' selected="selected" ':'').'>'.$language['ANY'].'</option>';
-		for ($value=0;$value <= ($cutoff*10);$value++) {
-			$cur=($value/10);
-			$combo.="\n".'<option value="'.$cur.'"'.($ratio==$cur?' selected="selected" ':'').'>'.$cur.'</option>';
-		}
-		$combo.="\n".'</select>';
-		$masspm['combo_from_ratio']=$combo;
-		# get ratio type
-		$combo="\n".'<select name="pick">';
-		$combo.="\n".'<option value="0"'.($pick==0?' selected="selected" ':'').'>'.$language['RATIO_ONLY'].'</option>';
-		$combo.="\n".'<option value="1"'.($pick==1?' selected="selected" ':'').'>'.$language['RATIO_GREAT'].'</option>';
-		$combo.="\n".'<option value="2"'.($pick==2?' selected="selected" ':'').'>'.$language['RATIO_LOW'].'</option>';
-		$combo.="\n".'</select>';
-		$masspm['combo_pick_ratio']=$combo;
-		$masspm['body']=textbbcode('masspm','msg',$msg);
-		$block_title=$language['ACP_MASSPM'];
-		break;
+          default:
+                $error=false;
+        }
+        # init options
+        $opts['name']='level';
+        $opts['complete']=true;
+        $opts['id']='id';
+        $opts['value']='level';
+        $opts['default']=$flevel;
+        # get from rank group
+        $ranks=rank_list();
+        $ranks[]=array('id'=>0, 'level'=>$language['ALL']);
+        $masspm['combo_from_group']=get_combo($ranks, $opts);
+        # get to rank group
+        $opts['name']='levell';
+        $opts['default']=$tlevel;
+        $masspm['combo_to_group']=get_combo($ranks, $opts);
+        # get ratios
+        $combo="\n".'<select name="ratio"><option value="0"'.($ratio==0?' selected="selected" ':'').'>'.$language['ANY'].'</option>';
+        for ($value=0;$value <= ($cutoff*10);$value++) {
+            $cur=($value/10);
+            $combo.="\n".'<option value="'.$cur.'"'.($ratio==$cur?' selected="selected" ':'').'>'.$cur.'</option>';
+        }
+        $combo.="\n".'</select>';
+        $masspm['combo_from_ratio']=$combo;
+        # get ratio type
+        $combo="\n".'<select name="pick">';
+        $combo.="\n".'<option value="0"'.($pick==0?' selected="selected" ':'').'>'.$language['RATIO_ONLY'].'</option>';
+        $combo.="\n".'<option value="1"'.($pick==1?' selected="selected" ':'').'>'.$language['RATIO_GREAT'].'</option>';
+        $combo.="\n".'<option value="2"'.($pick==2?' selected="selected" ':'').'>'.$language['RATIO_LOW'].'</option>';
+        $combo.="\n".'</select>';
+        $masspm['combo_pick_ratio']=$combo;
+        $masspm['body']=textbbcode('masspm','msg',$msg);
+        $block_title=$language['ACP_MASSPM'];
+        break;
 
-	default:
-		# invalid action
-		redirect('index.php?page=admin&user='.$CURUSER['uid'].'&code='.$CURUSER['random']);
+    default:
+        # invalid action
+        redirect('index.php?page=admin&user='.$CURUSER['uid'].'&code='.$CURUSER['random']);
 }
 
 $admintpl->set('frm_error',$error,true);
