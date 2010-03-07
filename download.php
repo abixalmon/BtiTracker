a29f6f2c700d0adb85e37fd032c15dad032b2dc7
petr1fied <petr1fied@8c072486-f8c7-49fb-bba5-1a7a9737b6d2>
2010-03-07 10:08:20 +0000

    - Fixes a possible sql injection hole - Fixes a language issue with the verification email

--- download.php
+++ download.php
@@ -47,7 +47,7 @@ if (!$CURUSER || $CURUSER["can_download"]=="no")
 if(ini_get('zlib.output_compression'))
   ini_set('zlib.output_compression','Off');
 
-$infohash=$_GET["id"];
+$infohash=mysql_real_escape_string($_GET["id"]);
 $filepath=$TORRENTSDIR."/".$infohash . ".btf";
 
 if (!is_file($filepath) || !is_readable($filepath))
--- include/xbtit_version.php
+++ include/xbtit_version.php
@@ -1,3 +1,3 @@
 <?php
-$tracker_version='2.0.0'; # revision 574 or newer
+$tracker_version='2.0.0'; # revision 575 or newer
 ?>
